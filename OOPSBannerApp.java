d1895dab56f3fccc5cf35568d731a3c39396220c
sr7173-ac <sr7173@srmist.edu.in>
2026-02-17 21:08:12 +0530

    UC2: Print OOPS in Banner Format using print statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -7,13 +7,13 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *      ");
-        System.out.println("*     * *     * *     * *      ");
-        System.out.println("*     * *     * * * *    ***** ");
-        System.out.println("*     * *     * *             *");
-        System.out.println("*     * *     * *             *");
-        System.out.println(" *****   *****  *        ***** ");
 
+        System.out.println(""+"  ****  "+"  ****  "+ " *****  "+ "  **** ");
+        System.out.println(""+" **  ** "+" **  ** "+ " **  ** "+ " **  ** ");
+        System.out.println(""+" **  ** "+" **  ** "+ " **  ** "+ " **     ");
+        System.out.println(""+" **  ** "+" **  ** "+ " ****** "+ " ***** ");
+        System.out.println(""+" **  ** "+" **  ** "+ " **     "+ "     ** ");
+        System.out.println(""+" **  ** "+" **  ** "+ " **     "+ " **  ** ");
+        System.out.println(""+"  ****  "+"  ****  "+ " **     "+ "  **** ");
     }
 }
