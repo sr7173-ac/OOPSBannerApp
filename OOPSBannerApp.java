85368b72f2e39d6618d57fc207436e657da44593
sr7173-ac <sr7173@srmist.edu.in>
2026-02-22 10:32:05 +0530

    UC3: Using String join

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -4,16 +4,16 @@
  * @author Shreya
  * @version 2.0
  */
-public class OOPSBannerApp {
+}public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *      ");
-        System.out.println("*     * *     * *     * *      ");
-        System.out.println("*     * *     * * * *    ***** ");
-        System.out.println("*     * *     * *             *");
-        System.out.println("*     * *     * *             *");
-        System.out.println(" *****   *****  *        ***** ");
 
+        System.out.println(String.join("","  ****  ","  ****  ", " *****  ", "  **** "));
+        System.out.println(String.join(""," **  ** "," **  ** ", " **  ** ", " **  ** "));
+        System.out.println(String.join(""," **  ** "," **  ** ", " **  ** ", " **     "));
+        System.out.println(String.join(""," **  ** "," **  ** ", " ****** ", " ***** "));
+        System.out.println(String.join(""," **  ** "," **  ** ", " **     ", "     ** "));
+        System.out.println(String.join(""," **  ** "," **  ** ", " **     ", " **  ** "));
+        System.out.println(String.join("","  ****  ","  ****  ", " **     ", "  **** "));
     }
 }
